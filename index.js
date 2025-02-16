300f66d0e24a388a28fb22faa7301c182f4a6e41
_004 <art@Arturs-MacBook-Pro.local>
2025-02-16 12:59:58 +0100

    Исправления отображения таймеров и хаков

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
 
